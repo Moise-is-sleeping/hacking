07605d857c8e8689726cf754fa62497614bc5fa0
Moise-is-sleeping <mbrebre1909@g.educaand.es>
2023-11-13 17:32:24 +0100

    hack

---
+++ hacking/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+string password = "!@#$%12345qwert";
+string incriptedPassword = Incrypt(password);
+BruteForce(incriptedPassword);
+
+
+void BruteForce(string IUserPassword)
+{
+    string[] passwords = ReadFile();
+    foreach (var psswd in passwords)
+    {
+        if (Incrypt(psswd) == IUserPassword)
+        {
+            Console.WriteLine("Password = "+psswd);
+            break;
+        }
+    }
+
+}
+
+string[] ReadFile()
+{
+    string[] passwords = File.ReadAllLines("C:\\Users\\usuariot\\RiderProjects\\hacking\\hacking\\passwords");
+    return passwords;
+}
+
+string Incrypt(string str)
+{
+    SHA256 sha256 = SHA256Managed.Create();
+    ASCIIEncoding encoding = new ASCIIEncoding();
+    byte[] stream = null;
+    StringBuilder sb = new StringBuilder();
+    stream = sha256.ComputeHash(encoding.GetBytes(str));
+    for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+    return sb.ToString();
+}
+
+
+
+
+
+
